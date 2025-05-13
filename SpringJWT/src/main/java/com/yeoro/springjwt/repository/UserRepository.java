d72bc685667d1b586397293677a0e5d133ae4722
yeoro <dufgh1009@gmail.com>
2025-05-13 15:57:41 +0900

    feat: DB연결 및 Entity 작성

---
+++ SpringJWT/src/main/java/com/yeoro/springjwt/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.yeoro.springjwt.repository;
+
+import com.yeoro.springjwt.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+}
