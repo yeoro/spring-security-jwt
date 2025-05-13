3d494113fe034679d7b15221f13abc596633ebfe
yeoro <dufgh1009@gmail.com>
2025-05-13 16:11:25 +0900

    feat: 회원가입 로직 구현

---
+++ SpringJWT/src/main/java/com/yeoro/springjwt/controller/JoinController.java
@@ -0,0 +1,23 @@
+package com.yeoro.springjwt.controller;
+
+import com.yeoro.springjwt.dto.JoinDTO;
+import com.yeoro.springjwt.service.JoinService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class JoinController {
+
+    private final JoinService joinService;
+
+    @PostMapping("/join")
+    public String joinProcess(JoinDTO joinDTO) {
+
+        joinService.joinProcess(joinDTO);
+
+        return "ok";
+    }
+
+}
